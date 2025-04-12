614375e09f52f33f0f9cfc41d2137f40367a2f8b
Neetesh Kahar <neetesh.kahar@ad.infosys.com>
2025-04-12 16:52:13 +0530

    use asynchronous programming

---
+++ Day4Problem/asynchronous_programming/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+public class Program
+{
+    // Simulate delay involved in a movie download using Thread.Sleep()
+    static string MovieDownload()
+    {
+        Console.WriteLine("Download started...");
+        Thread.Sleep(5000); // Simulate download delay
+        Console.WriteLine("Download finished...");
+        return "1024 MB downloaded";
+    }
+
+    // Method to read the review
+    static void ReadReview()
+    {
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("Review: This is a movie review!");
+        Console.WriteLine("-------------------------------");
+    }
+
+    // Main method
+    public static void Main()
+    {
+        Console.WriteLine(MovieDownload());
+        Console.WriteLine("Your download is in progress. Do you want to read the review? {Y/N}");
+        if (Console.ReadKey(true).KeyChar == 'y' || Console.ReadKey(true).KeyChar == 'Y')
+        {
+            ReadReview();
+        }
+        Console.WriteLine("Goodbye.");
+    }
+}
